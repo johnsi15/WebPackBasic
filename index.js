fd4842e0e8b9e36eb2873ad813662a8a1c317190
John serrano <andrey_201015@hotmail.com>
2017-09-06 17:27:28 -0500

    Cambios pequeños

--- index.js
+++ index.js
@@ -14,4 +14,4 @@ app.use(express.static(__dirname + '/public'));
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server on port ${port}`)
-})
+});
--- webpack.config.js
+++ webpack.config.js
@@ -92,7 +92,7 @@ const path = require('path');
 const config = {
   entry: './src',
   output: {
-    path: path.resolve(__dirname, '/'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
   devtool: 'source-map',
